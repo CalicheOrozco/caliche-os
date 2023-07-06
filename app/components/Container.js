d6fce110e0a6aed1e00be2c93e6a18f883c8219f
Caliche Orozco <caliche2da1@gmail.com>
2023-07-06 10:06:34 -0600

    remove: google analytics

--- app/components/Container.js
+++ app/components/Container.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { createContext } from 'react';
-import Script from "next/script";
 import dynamic from 'next/dynamic'
 
 
@@ -70,21 +69,6 @@ function Container({ children }) {
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
-        />
-
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-G6J4VPVBQV', {
-          page_path: window.location.pathname,
-          });
-        `,
-          }}
         />
         <div
           className={`flex-grow justify-center ${isVisible ? "" : "hidden"}`}
