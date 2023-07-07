6732138fcb8e6779cf3e86502982e2f3552822ad
Caliche Orozco <caliche2da1@gmail.com>
2023-07-07 13:25:41 -0600

    fix: handle click

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -8,7 +8,21 @@ import Image from "next/image";
 
 const Navbar = () => {
   const pathname = usePathname();
-  
+  const {
+    setisVisible,
+    setShowNavbar,
+  } = useContext(showNavbarContext);
+
+  const handleClick = (url) => {
+
+    if (pathname === url ){
+      setShowNavbar(false);
+      setisVisible(true);
+    }
+    
+  };
+
+
 
   return (
     <nav className={`min-h-screen flex justify-center items-center lg:justify-start `}>
@@ -16,7 +30,7 @@ const Navbar = () => {
       <ul className="flex flex-col gap-4 lg:p-10">
         {myData.navmenu.map((item, index) => (
           <li  key={`navbar-item-${index}`} >
-            <Link href={item.url}>
+            <Link href={item.url} onClick={() =>{handleClick(item.url)}}>
               <div
                 className={`flex flex-col items-center p-2 rounded-md ${
                   pathname.startsWith(`${item.url}`)
