f5868b7cd57f131725e2fa80d50db9cd815f0667
Caliche Orozco <caliche2da1@gmail.com>
2023-07-27 23:35:33 -0600

    future:added tailwindcss-animated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,28 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      animation: {
-        'fade-in': 'example-enter 500ms ease-in',
-        'fade-out': 'example-leave 300ms ease-in',
-      },
-      keyframes: {
-        'example-enter': {
-          '0%': { opacity: '0.01' },
-          '100%': { opacity: '1' },
-        },
-        'example-leave': {
-          '0%': { opacity: '1' },
-          '100%': { opacity: '0.01' },
-        },
-      },
-    },
-  },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
 };
