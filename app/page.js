77aee242e0631f07b02da1bd77f9d59218d9c56b
Caliche Orozco <caliche2da1@gmail.com>
2023-08-27 01:21:15 -0600

    update: nextUI config

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Container from "./components/Container";
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="relative max-h-screen">
-      <Container />
-    </main>
+    <NextUIProvider>
+      <main className="relative max-h-screen">
+        <Container />
+      </main>
+    </NextUIProvider>
   );
 }
