24698781b91954ee9a51ae03015a44b92e31c66c
Caliche Orozco <caliche2da1@gmail.com>
2023-08-27 01:22:48 -0600

    uodate: prop for cards

--- app/Projects/page.js
+++ app/Projects/page.js
@@ -1,35 +1,32 @@
 import Container from "../components/Container";
 import Window from "../components/Window";
 import myData from "@/constants/data";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 import ProjectSkeletoon from "../Skeletoon/ProjectSkeletoon";
 
-const DynamicCard = dynamic(() => import('../components/Card'), {
-  loading: () => (
-    <ProjectSkeletoon />
-  ),
-})
-
+const DynamicCard = dynamic(() => import("../components/Card"), {
+  loading: () => <ProjectSkeletoon />,
+});
 
 export default function Home() {
   return (
     <main className="relative max-h-screen">
       <Container noIndex={true}>
-      <Window title="Projects">
-        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
-          {myData.projects.map((proj, key) => (
-            <DynamicCard
-              title={proj.title}
-              link={proj.link}
-              imgUrl={proj.imgUrl}
-              github={proj.github} 
-              key={key}
-            />
-          ))}
-        </div>
-      </Window>
-
-    </Container>
+        <Window title="Projects">
+          <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
+            {myData.projects.map((proj, key) => (
+              <DynamicCard
+                title={proj.title}
+                link={proj.link}
+                imgUrl={proj.imgUrl}
+                github={proj.github}
+                stack={proj.stack}
+                key={key}
+              />
+            ))}
+          </div>
+        </Window>
+      </Container>
     </main>
   );
 }
